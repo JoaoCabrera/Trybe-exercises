043a6a584188097af5e10a73cd298486bd63fd32
JoaoCabrera <joaocabrerati@gmail.com>
2021-04-10 14:08:23 -0300

    Resolve execicios 7, 8 e 9

---
+++ exercises/4.2/ex07.js
@@ -0,0 +1,8 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let smaller = numbers[0];
+for (i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] < smaller) {
+    smaller = numbers[i];
+  }
+}
+console.log(smaller);
---
+++ exercises/4.2/ex09.js
@@ -0,0 +1,7 @@
+let array = [];
+for (i = 0; i <=25; i += 1) {
+  array.push(i)
+}
+for (i = 0; i < array.length; i += 1) {
+  console.log(array[i] / 2);
+}
